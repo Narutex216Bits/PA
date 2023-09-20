668d9c62ff1cd0a5392b8cacc1e17b5883f3bd67
Aluno <aluno@etec.sp.gov.br>
2023-09-19 22:48:43 -0300

    Atividade Aula 19/09/2023

---
+++ PA/Aula_19_09_2023/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula_19_09_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração das Variáveis //
+
+            float media, nota0, nota1, nota2, nota3;
+
+            // Entrada de dados //
+
+            Console.Write("Informe a 1ª Nota: ");
+            nota0 = float.Parse(Console.ReadLine());
+        }
+    }
+}
