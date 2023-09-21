d77acddb7dea1a88d31ff516800e9ea2a14c95a4
Aluno <aluno@etec.sp.gov.br>
2023-09-21 20:46:44 -0300

    Atividades Aula 21/09/2023

--- PA/Aula_19_09_2023/Program.cs
+++ PA/Aula_19_09_2023/Program.cs
@@ -14,6 +14,39 @@ namespace Aula_19_09_2023
 
             Console.Write("Informe a 1ª Nota: ");
             nota0 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a 2ª Nota: ");
+            nota1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a 3ª Nota: ");
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a 4ª Nota: ");
+            nota3 = float.Parse(Console.ReadLine());
+
+            // Processamento dos dados //
+
+            media = (nota0 + nota1 + nota2 + nota3) / 4;
+                  
+            // Informação da média e se o aluno foi aprovado //
+
+            if (media < 5)
+            {
+                Console.WriteLine("Reprovado!!!");
+                Console.WriteLine("Temos mais um Repetente!!!");
+            }
+            else if (media < 7)
+            {
+                Console.WriteLine("Recuperação!!!");
+                Console.WriteLine("Mais um aluno que vem na escola até o Natal!!!");
+            }
+            else
+            {
+                Console.WriteLine("Aprovado!!!");
+                Console.WriteLine("Parabóins!!! Você não tem vida social!!!");
+            }
+
+
         }
     }
 }
