f528ee67193eb481719414e5a85c5f56249b2d61
Marcio Henrique de Tulio <49996462+Narutex216Bits@users.noreply.github.com>
2023-11-09 21:20:10 -0300

    Aula 09_11_2023_Ex01
    Aula 09/11/2023 Ex01 Programação e Algoritmo: Funções.

---
+++ PA/Aula_09_11_2023_Ex01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Aula_09_11_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta = "S";
+            
+            while (resposta.ToUpper() == "S")
+            {
+                Console.Clear();
+
+                CalcularTabuada();
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Deseja continuar? ");
+                resposta = Console.ReadLine();
+            }
+                       
+        }
+        static void CalcularTabuada()
+        {
+            int fator, limite;
+
+            Console.WriteLine("Digite o fator desejado: ");
+            fator = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o limite da tabuada: ");
+            limite = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador <= limite; contador++)
+            {
+                if (contador % 2 == 0)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine($"{fator} x {contador} = {fator * contador}");
+            }
+        }
+    }
+}
