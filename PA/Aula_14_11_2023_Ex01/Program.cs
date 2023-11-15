61352957483cc5c077178d95d121da896724c3bc
Aluno <aluno@etec.sp.gov.br>
2023-11-14 22:49:14 -0300

    Aula 14 11 2023 Exercicios

---
+++ PA/Aula_14_11_2023_Ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula_14_11_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num0, num1;
+            
+            for(int contador = 1; contador <= 3; contador++)
+            {
+                Console.WriteLine("Digite o primeiro valor da subtração: ");
+                num0 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o Segundo valor da subtração: ");
+                num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"O resultado da Subtração é: {num0 - num1}");
+            }
+
+            Console.WriteLine("Fim das contas!");
+        }
+    }
+}
---
+++ PA/Aula_14_11_2023_Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula_14_11_2023_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limite;
+
+            Console.WriteLine("Digite o número desejado: ");
+            limite = int.Parse(Console.ReadLine());
+
+            for(int contador = 1; contador <= limite; contador++)
+            {
+                Console.WriteLine($"{contador % 2 == 1}");
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+        }
+    }
+}
