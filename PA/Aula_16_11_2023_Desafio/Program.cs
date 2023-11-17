d235c1849af242a971fd6b646720a9bef99a0a92
Marcio Henrique de Tulio <49996462+Narutex216Bits@users.noreply.github.com>
2023-11-16 22:53:53 -0300

    Add files via upload

---
+++ PA/Aula_16_11_2023_Desafio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula_16_11_2023_Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 1;
+            int totalIdade = 0;
+            int qtdeIdadeDigitada = 0;
+
+            while (idade != 0)
+            {
+                Console.Write("Digite a idade da pessoa: ");
+                idade = int.Parse(Console.ReadLine());
+
+                if (idade == 0)
+                    break;
+                
+                //totalIdade = totalIdade + Idade;
+                totalIdade += idade;
+
+                qtdeIdadeDigitada++;
+                
+            }
+
+            Console.WriteLine($"Média das Idades: {totalIdade / qtdeIdadeDigitada}");
+        }
+    }
+}
